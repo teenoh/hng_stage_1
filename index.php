e0d140b020cfa28a53d3a88a872241810799d0a7
teenoh <valentino@grit.systems>
2018-04-09 01:15:43 +0100

    add html, css files and image

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Stage 1 task</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="index.css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+
+<body>
+    <section class="full-screen">
+        <p class="time"><?php echo date("h:i");?></p>
+        <p class="greeting">Good evening teenoh</p>
+        <p class="question">What is your main focus for today</p>
+        <hr />
+        <p class="quote"><em>"it takes as much energy to wish as to plan"</em></p>
+    </section>
+
+    <!-- <script>
+        var d = new Date()
+        document.querySelector(".time").innerHTML = `${d.getHours()} : ${d.getMinutes()}`
+    </script> -->
+</body>
+
+</html>
